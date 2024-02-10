733227ddde832df7e48a097e40bbc6ead2973fbe
honorablement <143546481+Vacpize@users.noreply.github.com>
2024-02-10 10:56:04 +0200

    code.py

--- Ping_Pong/code.py
+++ Ping_Pong/code.py
@@ -1,4 +1,5 @@
 from pygame import *
+from time import time as tim
 
 wind = display.set_mode((600,500))
 background = transform.scale(image.load('back.png'), (600, 500))
@@ -9,9 +10,11 @@ mixer.init()
 hit = mixer.Sound('hit.wav')
 
 font1 = font.SysFont("Montserrat-Blod.ttf", 50)
-end_font = font.SysFont("Montserrat-Blod.ttf", 20)
+end_font = font.SysFont("Montserrat-Blod.ttf", 35)
 end_txt = font1.render("The game ended", True, (224, 128, 40))
-bottom_txt = end_font.render("good job", True, (224, 128, 40))
+bottom_loose = end_font.render("You loose, bro", True, (224, 128, 40))
+bottom_win = end_font.render("You win, bro", True, (224, 128, 40))
+
 
 clock = time.Clock()
 class GameSprite(sprite.Sprite):
@@ -54,23 +57,28 @@ class Player(GameSprite):
 player1 = Player("platform.png", 550, 169, 5, 40, 162)
 player2 = Player("platform.png", 20, 169, 5, 40, 162)
 
-ball = GameSprite("ball.png", 300, 250, 2, 80,80)
-speed_y = 2
-speed_x = 2
-follow = False
+ball = GameSprite("ball.png", 250, 250, 2, 80,80)
+speed_y = 3
+speed_x = 3
 
+start_time = tim()
+true_time = None
+
+follow = False
 game = True
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-
+    print(ball.rect.x)
     clock.tick(60)
 
         # update location of sprites
-
+    # print(game)
     player1.moving1()
     player2.moving2()
+    
+    #ball's phisycs
     if ball.rect.y >= 0 and ball.rect.bottom <= 500 and ball.rect.x >= 0 and ball.rect.x < 600 - 80:
         if ball.colliderect(player1.rect):
             speed_x *= -1
@@ -86,24 +94,43 @@ while game:
             speed_y *= -1
         if ball.rect.x < -3 or ball.rect.x > 530:
             follow = True
+            print(ball.rect.x)
+            print(follow)
             game = False
 
     ball.rect.x += speed_x
     ball.rect.y += speed_y
 
-    #update/draw sprites
+    # update time
+    end_time = tim()
+    true_time = round(end_time - start_time)
+    # text of time 
+    txt = f'Time:{str(true_time)}'
+    time_txt = font1.render(txt, True, (224, 128, 40))
+    # check, if game must end (win)
+    if true_time >= 100:
+        game = False 
+        follow = True
+        # print(follow)
+        #update/draw sprites
 
     wind.blit(background, (0,0))
     ball.reset()
     player1.reset()
     player2.reset()
-
+    wind.blit(time_txt, (10, 30))
     display.update()
 
+#if game ended(loose)
 while follow:
     for e in event.get():
         if e.type == QUIT:
             follow = False
-    wind.blit(end_txt, (160, 200))
-    wind.blit(bottom_txt, (270, 250))
+    if ball.rect.x < -3 or ball.rect.x > 530:
+        wind.blit(end_txt, (160, 200))
+        wind.blit(bottom_loose, (250, 250))
+    
+    if true_time > 120:
+        wind.blit(end_txt, (160, 200))
+        wind.blit(bottom_win, (250, 220))
     display.update()
